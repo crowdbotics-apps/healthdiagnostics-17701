5bccf19030dfcc688914f0b6913e4cb1b0df36c5
Crowdbotics <team@crowdbotics.com>
2020-06-02 12:07:12 +0000

    Update models

--- backend/healthdiagnostics_17701/wsgi.py
+++ backend/healthdiagnostics_17701/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthdiagnostics_17701.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "healthdiagnostics_17701.settings")
 
 application = get_wsgi_application()
